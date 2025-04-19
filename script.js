6c8fcf7fcb14827fb8677d7f2f50020db4be4a0f
Reyrove <160649999+reyrove@users.noreply.github.com>
2025-04-19 04:30:50 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll('nav ul li a');
+
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop - 60,
+        });
+    });
+});
